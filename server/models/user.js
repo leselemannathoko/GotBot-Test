83a287147f3e082c67b40b52122ff1beb29ac448
Lesele Mannathoko <Lesele Mannathoko@DESKTOP-S34IRVS>
2020-06-22 15:02:02 +0200

    fixing the latest Api

--- server/models/user.js
+++ server/models/user.js
@@ -5,10 +5,6 @@ const userSchema = new Scheema({
 
     name: String,
     email: String,
-<<<<<<< HEAD
-=======
-    name: String,
->>>>>>> 56acb0b83b70ea787cfe85be16ab6a51b8f1a384
     mobile: String
 })
 module.exports = mongoose.model('user', userSchema, 'People')
