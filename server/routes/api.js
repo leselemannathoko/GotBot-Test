25eebddc4048068459fd2fd9e52e51db88ab31cd
Lesele Mannathoko <Lesele Mannathoko@DESKTOP-S34IRVS>
2020-06-25 11:09:42 +0200

    Working Api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017',{ useUnifiedTopology: true, useNewUrlParser: true  } );
+const mongoose = require('mongoose');
+const { ObjectId } = require('mongodb');
+const { json } = require('body-parser');
+mongoose.connect('mongodb://localhost:27017',{ useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false } );
 //const db = "mongodb+srv://test:1234@cluster0-hdjuw.mongodb.net/Users?retryWrites=true&w=majority"
 // mongoose.connect(db, err => {
 
@@ -21,7 +23,7 @@ router.get('/', (req, res) => {
 });
 
 router.post('/register', (req, res) => {
-    let userData = req.body
+    let userData = req.body;
     let user = User(userData)
     user.save((error, registerdUser) => {
 
@@ -36,7 +38,7 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/fetch', (req, res) => {
-    let userData = req.body
+    let userData = req.body;
     User.findOne({email: userData.email}, (err, user) => {
       if (err) {
         console.log(err)    
@@ -66,21 +68,17 @@ router.post('/fetch', (req, res) => {
    });
 
    router.put('/update/:id',async(req,res) =>{
-     user = await User.findById(req.params.id)
-     user.name = req.body.name;
-     user.email = req.body.email;
-     user.mobile = req.body.mobile;
-
-     user.save((error, registerdUser) => {
-
-      if (error){
-          console.log(error)
-      }
-      else{
-          res.status(200).send(registerdUser)
-      }
+   if (!ObjectId.isValid(req.params.id))
+       return res.status(400).send(`No Records Of given Id was found : ${req.params.id}`);
+   
+       let userData = req.body;
       
-  })
-   }) 
+      User.findByIdAndUpdate(req.params.id, {$set: userData}, {new: true}, (err, doc)=>{
+        if(!err) {res.send(doc);}
+      else{console.log(`Error in User Update: ` + JSON.stringify(err, undefined, 2));}
+      });
+      
+
+      }); 
 
 module.exports = router
